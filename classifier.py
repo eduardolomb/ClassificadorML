884d3d85ad369dde84c3e0734c4f0ab8b33b037f
Eduardo Lombardi <eduardoribeiro@brq.com>
2018-04-20 11:18:12 -0300

    Alteradas imagens e tipos para rastreamento

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@ url = "dataset/"
 
 data = turi.image_analysis.load_images(url)
 
-data["imageType"] = data["path"].apply(lambda path: "Arvore" if "arvore" in path else "Casa" if "casa" in path else "Celular" if "celular" in path else "Computador" if "computador" in path else "Copo" if "copo" in path else "Floresta" if "floresta" in path else "Monitor" if "monitor" in path else "Mouse" if "mouse" in path else "Piscina" if "piscina" in path else "Android" if "android" in path else "iPhone")
+data["imageType"] = data["path"].apply(lambda path: "Android" if "android" in path else "Arvore" if "arvore" in path else "Cadeira" if "cadeira" in path else "Casa" if "casa" in path else "Celular" if "celular" in path else "Computador" if "computador" in path else "Copo" if "copo" in path else "Floresta" if "floresta" in path else "iPhone" if "iPhone" in path else "Mesa" if "mesa" in path else "Monitor" if "monitor" in path else "Mouse" if "mouse" in path else "Paisagem" if "paisagem" in path else "Piscina" if "piscina" in path else "Praia" if "praia" in path else "Televisão")
 
 
 
